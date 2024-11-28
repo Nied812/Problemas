4d4bbf17e1fbe732771ff5301a1ca995fbc6c095
Nied812 <niedfersonjose@gmail.com>
2024-11-27 21:46:58 -0500

    message

---
+++ Entregable2.py
@@ -0,0 +1,9 @@
+## Problema numero 1 Niedferson Vargas
+
+vaca = 1
+metros = int(input("Cuantos metros cuadrados tiene de pasto?: "))
+
+leche_por_dia = vaca * metros
+
+leche_semanal = leche_por_dia * 7
+print(f"a la semana se producen {leche_semanal} litros")
